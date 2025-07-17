44a03ffb56f05c3249d55b4b8fc8db4d902f225f
David <dchibuzor31@gmail.com>
2025-07-17 02:58:36 +0100

    updated links and teamlead

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function scrollGallery() {
   const scrollBox = document.getElementById("galleryScroll");
   scrollBox.scrollBy({ left: 300, behavior: "smooth" });
 }
+
+function toggleReadMore(btn) {
+  const moreText = btn.previousElementSibling.querySelector(".more-text");
+  const isVisible = moreText.style.display === "inline";
+
+  if (isVisible) {
+    moreText.style.display = "none";
+    btn.textContent = "Read More";
+  } else {
+    moreText.style.display = "inline";
+    btn.textContent = "Show Less";
+  }
+}
